b99dd0dbde11364ac74c790d2cc29d8360321ba3
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-30 17:02:46 -0400

    add more vlc plugins

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,20 +7,23 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	kaffeine          \
-	kvantum-qt5       \
-	pipewire-audio    \
-	pipewire-jack     \
-	qt5ct             \
-	qt5-wayland       \
-	vlc-plugin-aalib  \
-	vlc-plugin-aom    \
-	vlc-plugin-ass    \
-	vlc-plugin-avahi  \
-	vlc-plugin-bluray \
-	vlc-plugin-caca   \
-	vlc-plugin-dvd    \
-	vlc-plugin-ffmpeg
+	kaffeine            \
+	kvantum-qt5         \
+	pipewire-audio      \
+	pipewire-jack       \
+	qt5ct               \
+	qt5-wayland         \
+	vlc-plugin-aalib    \
+	vlc-plugin-aom      \
+	vlc-plugin-ass      \
+	vlc-plugin-avahi    \
+	vlc-plugin-bluray   \
+	vlc-plugin-caca     \
+	vlc-plugin-dvd      \
+	vlc-plugin-ffmpeg   \
+	vlc-plugin-freetype \
+	vlc-plugin-srt      \
+	vlc-plugin-svg
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
